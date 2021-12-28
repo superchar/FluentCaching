0fa685483380058bbd11a657b41dc8af42d53c34
Vladyslav Lobyntsev <lobintsevvladislav@gmail.com>
2021-12-28 11:31:13 +0200

    Unit tests for CacheImplementationBuilder and refactoring

---
+++ FluentCaching.Tests.Unit/PolicyBuilders/CacheImplementationBuilderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using FluentCaching.Cache;
+using FluentCaching.Cache.Models;
+using FluentCaching.PolicyBuilders;
+
+namespace FluentCaching.Tests.Unit.PolicyBuilders
+{
+    public class CacheImplementationBuilderTests
+    {
+        private readonly CacheOptions _cacheOptions;
+
+        private readonly CacheImplementationBuilder _sut;
+
+        public CacheImplementationBuilderTests()
+        {
+            _cacheOptions = new CacheOptions();
+
+            _sut = new CacheImplementationBuilder(_cacheOptions);
+        }
+
+        [Fact]
+        public void WithCacheImplementation_CacheImplementationIsNull_ThrowsException()
+        {
+            _sut.Invoking(s => s.WithCacheImplementation(null)).Should()
+                .Throw<ArgumentNullException>().WithMessage("Cache implementation cannot be null (Parameter 'cacheImplementation')");
+        }
+
+        [Fact]
+        public void WithCacheImplementation_CacheImplementationIsNotNull_SetsCacheImplementation()
+        {
+            var cacheImplementationMock = new Mock<ICacheImplementation>();
+
+            _sut.WithCacheImplementation(cacheImplementationMock.Object);
+
+            _cacheOptions.CacheImplementation.Should().NotBeNull();
+        }
+    }
+}
--- FluentCaching/PolicyBuilders/CacheImplementationBuilder.cs
+++ FluentCaching/PolicyBuilders/CacheImplementationBuilder.cs
@@ -1,11 +1,12 @@
 ﻿using FluentCaching.Cache;
 using FluentCaching.Cache.Models;
+using System;
 
 namespace FluentCaching.PolicyBuilders
 {
     public class CacheImplementationBuilder
     {
-        private CacheOptions _currentOptions;
+        private readonly CacheOptions _currentOptions;
 
         public CacheImplementationBuilder(CacheOptions currentOptions)
         {
@@ -16,7 +17,8 @@ namespace FluentCaching.PolicyBuilders
 
         public CacheImplementationBuilder WithCacheImplementation(ICacheImplementation cacheImplementation)
         {
-            _currentOptions.CacheImplementation = cacheImplementation;
+            _currentOptions.CacheImplementation = cacheImplementation
+                ?? throw new ArgumentNullException(nameof(cacheImplementation), "Cache implementation cannot be null");
             return this;
         }
     }
