14d97e4152503d97e39617ad0d8db127d287057c
Vladyslav Lobyntsev <lobintsevvladislav@gmail.com>
2023-08-10 17:06:50 +0300

    Added more samples

--- samples/FluentCaching.Samples.DistributedCache/Program.cs
+++ samples/FluentCaching.Samples.DistributedCache/Program.cs
@@ -1,7 +1,8 @@
 using FluentCaching.Cache;
 using FluentCaching.DependencyInjectionExtensions;
+using FluentCaching.DistributedCache;
 using FluentCaching.Memory;
-using FluentCaching.Samples.AspNetCore.Models;
+using FluentCaching.Samples.DistributedCache.Models;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,12 +17,15 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 builder.Services.AddFluentCaching(cacheBuilder => cacheBuilder
     .For<Cart>(_ => _.UseAsKey(c => $"cart-{c.Id}").And().SetExpirationTimeoutTo(5).Minutes
-        .With().SlidingExpiration().And().StoreInMemory()));
+        .With().SlidingExpiration().And().StoreInMemory())
+    .For<UserCheckoutStatistics>(_ => _.UseClassNameAsKey().CombinedWith(s => s.UserId)
+        .And().SetInfiniteExpirationTimeout().And().StoreInDistributedCache()));
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseFluentCaching();
 
 app.MapPost("/cart-items", async ([FromBody] CartItemDto dto, ICache cache) =>
 {
@@ -40,6 +44,19 @@ app.MapPost("/cart-items", async ([FromBody] CartItemDto dto, ICache cache) =>
     await cache.CacheAsync(cart);
 });
 
-app.MapGet("/{cartId:guid}cart-items", (Guid cartId, ICache cache) => cache.RetrieveAsync<Cart>(cartId));
+app.MapGet("/{cartId:guid}/cart-items", (Guid cartId, ICache cache) => cache.RetrieveAsync<Cart>(cartId));
+
+app.MapPost("/{cartId:guid}/checkouts", async (Guid cartId, [FromQuery] Guid userId, ICache cache) =>
+{
+    await cache.RemoveAsync<Cart>(cartId);
+    var checkoutStatistics = await cache.RetrieveAsync<UserCheckoutStatistics>(userId)
+                             ?? new UserCheckoutStatistics(userId);
+    checkoutStatistics.CheckoutCount++;
+    await cache.CacheAsync(checkoutStatistics);
+});
+
+app.MapGet("/{userId:guid}/checkout-statistics",
+    (Guid userId, ICache cache) => cache.RetrieveAsync<UserCheckoutStatistics>(userId));
+
 
 app.Run();
